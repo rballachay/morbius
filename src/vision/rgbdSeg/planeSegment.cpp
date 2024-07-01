d802ab40fb8614ba384bf21da61e9b8cac564da3
rballachay <riley.ballachay@gmail.com>
2024-07-01 15:42:25 -0400

    remove optimization

--- src/vision/rgbdSeg/planeSegment.cpp
+++ src/vision/rgbdSeg/planeSegment.cpp
@@ -4,66 +4,6 @@
 // can be changed to an arg later
 #define AGENT_WIDTH 5 // cm
 
-PlaneDetection plane_detection;
-
-//-----------------------------------------------------------------
-// MRF energy functions
-MRF::CostVal dCost(int pix, int label)
-{
-	return plane_detection.dCost(pix, label);
-}
-
-MRF::CostVal fnCost(int pix1, int pix2, int i, int j)
-{
-	return plane_detection.fnCost(pix1, pix2, i, j);
-}
-
-void runMRFOptimization()
-{
-	DataCost *data = new DataCost(dCost);
-	SmoothnessCost *smooth = new SmoothnessCost(fnCost);
-	EnergyFunction *energy = new EnergyFunction(data, smooth);
-	int width = kDepthWidth, height = kDepthHeight;
-	MRF* mrf = new Expansion(width * height, plane_detection.plane_num_ + 1, energy);
-	// Set neighbors for the graph
-	for (int row = 0; row < height; row++)
-	{
-		for (int col = 0; col < width; col++)
-		{
-			int pix = row * width + col;
-			if (col < width - 1) // horizontal neighbor
-				mrf->setNeighbors(pix, pix + 1, 1);
-			if (row < height - 1) // vertical
-				mrf->setNeighbors(pix, pix + width, 1);
-			if (row < height - 1 && col < width - 1) // diagonal
-				mrf->setNeighbors(pix, pix + width + 1, 1);
-		}
-	}
-	mrf->initialize();
-	mrf->clearAnswer();
-	float t;
-	mrf->optimize(5, t);  // run for 5 iterations, store time t it took 
-	MRF::EnergyVal E_smooth = mrf->smoothnessEnergy();
-	MRF::EnergyVal E_data = mrf->dataEnergy();
-	cout << "Optimized Energy: smooth = " << E_smooth << ", data = " << E_data << endl;
-	cout << "Time consumed in MRF: " << t << endl;
-
-	// Get MRF result
-	for (int row = 0; row < height; row++)
-	{
-		for (int col = 0; col < width; col++)
-		{
-			int pix = row * width + col;
-			plane_detection.opt_seg_img_.at<cv::Vec3b>(row, col) = plane_detection.plane_colors_[mrf->getLabel(pix)];
-			plane_detection.opt_membership_img_.at<int>(row, col) = mrf->getLabel(pix);
-		}
-	}
-	delete mrf;
-	delete energy;
-	delete smooth;
-	delete data;
-}
-
 int findMinZVal(const std::vector<std::vector<int>>& plane_vertices, std::vector<VertexType>& vertices, std::vector<bool> candidates) {
     int minIndex = -1;
     int minValue = std::numeric_limits<int>::max(); // Start with a large value
@@ -294,8 +234,8 @@ void processMask(cv::Mat& mask, int dilationSize = 5, int erosionSize = 5) {
     cv::Mat elementErode = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(erosionSize, erosionSize));
 
     // Apply dilation followed by erosion
-    cv::dilate(mask, mask, elementDilate);
-    cv::erode(mask, mask, elementErode);
+    //cv::dilate(mask, mask, elementDilate);
+    //cv::erode(mask, mask, elementErode);
 
 	// helps to get continuous vector
 	fillHoles(mask);
@@ -398,11 +338,13 @@ cv::Mat drawDistanceVectors(cv::Mat image, PlaneDetection& plane_detection, Surf
 
 int realSenseAttached(){
 
+	PlaneDetection plane_detection;
+
     try {
         RealSense realsense;
         realsense.startPipeline();
 
-        realsense.captureFrames([](const rs2::frameset& frames) {
+        realsense.captureFrames([&plane_detection](const rs2::frameset& frames) {
             rs2::depth_frame depth = frames.get_depth_frame();
             rs2::video_frame rgb_frame = frames.get_color_frame();
 
@@ -450,6 +392,8 @@ int realSenseAttached(){
 int loadProcessDefault(){
 	std::string colorImagePath = "data/raw_image.png";
     std::string depthImagePath = "data/depth_image.png";
+
+	PlaneDetection plane_detection;
 
 	cv::Mat colorImage = cv::imread(colorImagePath, cv::IMREAD_COLOR);
 	cv::Mat depthImage = cv::imread(depthImagePath, cv::IMREAD_UNCHANGED);
