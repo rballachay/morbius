2e6a9eb74d54fd14a1169ed1775df6a534373bd6
rballachay <riley.ballachay@gmail.com>
2024-07-01 15:41:41 -0400

    add option to load from file + max traversible distance based on agent width

--- src/vision/rgbdSeg/planeSegment.cpp
+++ src/vision/rgbdSeg/planeSegment.cpp
@@ -1,6 +1,68 @@
 #include "plane_detection.h"
 #include "realsense.hpp"
 
+// can be changed to an arg later
+#define AGENT_WIDTH 5 // cm
+
+PlaneDetection plane_detection;
+
+//-----------------------------------------------------------------
+// MRF energy functions
+MRF::CostVal dCost(int pix, int label)
+{
+	return plane_detection.dCost(pix, label);
+}
+
+MRF::CostVal fnCost(int pix1, int pix2, int i, int j)
+{
+	return plane_detection.fnCost(pix1, pix2, i, j);
+}
+
+void runMRFOptimization()
+{
+	DataCost *data = new DataCost(dCost);
+	SmoothnessCost *smooth = new SmoothnessCost(fnCost);
+	EnergyFunction *energy = new EnergyFunction(data, smooth);
+	int width = kDepthWidth, height = kDepthHeight;
+	MRF* mrf = new Expansion(width * height, plane_detection.plane_num_ + 1, energy);
+	// Set neighbors for the graph
+	for (int row = 0; row < height; row++)
+	{
+		for (int col = 0; col < width; col++)
+		{
+			int pix = row * width + col;
+			if (col < width - 1) // horizontal neighbor
+				mrf->setNeighbors(pix, pix + 1, 1);
+			if (row < height - 1) // vertical
+				mrf->setNeighbors(pix, pix + width, 1);
+			if (row < height - 1 && col < width - 1) // diagonal
+				mrf->setNeighbors(pix, pix + width + 1, 1);
+		}
+	}
+	mrf->initialize();
+	mrf->clearAnswer();
+	float t;
+	mrf->optimize(5, t);  // run for 5 iterations, store time t it took 
+	MRF::EnergyVal E_smooth = mrf->smoothnessEnergy();
+	MRF::EnergyVal E_data = mrf->dataEnergy();
+	cout << "Optimized Energy: smooth = " << E_smooth << ", data = " << E_data << endl;
+	cout << "Time consumed in MRF: " << t << endl;
+
+	// Get MRF result
+	for (int row = 0; row < height; row++)
+	{
+		for (int col = 0; col < width; col++)
+		{
+			int pix = row * width + col;
+			plane_detection.opt_seg_img_.at<cv::Vec3b>(row, col) = plane_detection.plane_colors_[mrf->getLabel(pix)];
+			plane_detection.opt_membership_img_.at<int>(row, col) = mrf->getLabel(pix);
+		}
+	}
+	delete mrf;
+	delete energy;
+	delete smooth;
+	delete data;
+}
 
 int findMinZVal(const std::vector<std::vector<int>>& plane_vertices, std::vector<VertexType>& vertices, std::vector<bool> candidates) {
     int minIndex = -1;
@@ -28,20 +90,12 @@ struct Surfaces{
 	std::vector<ahc::PlaneSeg::shared_ptr> planes;
 	std::vector<vector<int>> plane_vertices;
 	std::vector<VertexType> vertices;
-	std::vector<int> angles;
 	std::vector<cv::Vec3b> colors;
 
 	size_t numPlanes;
 	int groundIdx; // index of the ground, if it can't be assigned, gives warning
 
 	Surfaces(const PlaneDetection& planeDetection) {
-        
-		// angles are heading directions, we are going to use these
-		// to calculate how far we can go in each direction. 
-		for (int angle = -45; angle <= 45; angle += 5) {
-            angles.push_back(angle);
-        }
-		
 		planes = planeDetection.plane_filter.extractedPlanes;
 		plane_vertices = planeDetection.plane_vertices_;
 		vertices = planeDetection.cloud.vertices;
@@ -94,6 +148,96 @@ struct Surfaces{
 
 };
 
+struct GroundVectors{
+	/* Container for holding the ground vectors and their max
+	distances so that we can calculate the max traversible distance
+	for each vector on the ground.
+	*/
+	ImagePointCloud cloud;
+	std::vector<int> vecMaxJ; // the initial max J value at each vector
+	std::vector<int> vecMaxI; // the initial max I row for each vector
+	int vecCount; 
+	int stride; 
+	double agentWidth; 
+
+	std::vector<int> _vecMaxZ; // the real max Z, considering the agent width
+	std::vector<int> _vecMaxI; // the real max I 
+
+    GroundVectors(int vecCount, int stride, double agentWidth = AGENT_WIDTH)
+        : vecCount(vecCount), stride(stride), agentWidth(agentWidth) {
+            vecMaxJ.resize(vecCount, 0); // Initialize vecMaxZ with vecCount elements set to 0.0
+            vecMaxI.resize(vecCount, 0); // Initialize vecMaxI with vecCount elements set to 0
+			_vecMaxZ.resize(vecCount, 0);
+			_vecMaxI.resize(vecCount, 0);
+        }
+
+	void finalize(cv::Mat mask){
+		// iterate over all vectors and see how far there is space
+		int halfW = agentWidth;
+
+		// check the width 
+		for (int k=0; k<vecCount; k++){
+			int iMax = vecMaxI[k];
+			int j = vecMaxJ[k];
+
+			int i;
+			int hitObject = false;
+			for (i = cloud.height()-1; i >= iMax; i--){
+				double x, y, z, xL, xR;
+				cloud.get(i, j, x, y, z);
+
+				// first check the right
+				int jR = j;
+				while (true){
+					jR++;
+					if (jR>cloud.width()-1){
+						break;
+					}
+					cloud.get(i, jR, xR, y, z);
+					if (abs(x-xR)>agentWidth){
+						break;
+					}
+					cv::Vec3b pixelValue = mask.at<cv::Vec3b>(i, jR);
+					if (!(std::abs(pixelValue[1] - 255) < 20 && pixelValue[0] < 20 && pixelValue[2] < 20)) {
+						hitObject=true;
+						break;
+					}
+				}
+
+				// next check the left
+				int jL = j;
+				while (true){
+					jL--;
+					if (jL<0){
+						break;
+					}
+					cloud.get(i, jL, xL, y, z);
+					if (abs(x-xL)>agentWidth){
+						break;
+					}
+					cv::Vec3b pixelValue = mask.at<cv::Vec3b>(i, jL);
+					if (!(std::abs(pixelValue[1] - 255) < 20 && pixelValue[0] < 20 && pixelValue[2] < 20)) {
+						hitObject=true;
+						break;
+					}
+				}
+
+				if (hitObject){
+					break;
+				}
+					
+			}
+
+			double x,y,z;
+			cloud.get(i, j, x, y, z);
+			_vecMaxZ[k] = z;
+			_vecMaxI[k] = i;
+
+		}
+	}
+
+};
+
 void printPlaneMembership(PlaneDetection& plane_detection){
 	std::cout << plane_detection.cloud.vertices.size() << std::endl;
 	int total = 0;
@@ -107,7 +251,6 @@ void printPlaneMembership(PlaneDetection& plane_detection){
 		}
 	}
 }
-
 
 void fillHoles(const cv::Mat& src) {
 	int kernelSize = 16;
@@ -163,9 +306,12 @@ cv::Mat drawDistanceVectors(cv::Mat image, PlaneDetection& plane_detection, Surf
     there are some random dots in the original mask which mess with the distance calculation. This
     is a hacky way of doing this, but its simple.
     */
-   	int stride = 32;
+   cv::Mat drawnImage = image.clone(); 
 
-	cv::Mat drawnImage = image.clone(); 
+   	if (surfaces.groundIdx==-1){
+		return drawnImage;
+	}
+
     processMask(drawnImage);
 
     // Define arrow properties
@@ -175,67 +321,88 @@ cv::Mat drawDistanceVectors(cv::Mat image, PlaneDetection& plane_detection, Surf
     int shift = 0; // Number of fractional bits in the point coordinates
     double tipLength = 0.1; // Length of the arrow tip in relation to the arrow length
 
-    if (surfaces.groundIdx > -1){
-        int col = plane_detection.cloud.width() / 2 - 1;
-		for (int maxJ = col-4*stride; maxJ<=col+4*stride; maxJ+=stride){
-			bool onSurface = false;
-			double maxZ = 0;
-			int maxI = -1;
-			for (int i = plane_detection.cloud.height()-1; i >= 0; i--){
-				// Access the pixel value
-				cv::Vec3b pixelValue = image.at<cv::Vec3b>(i, maxJ);
+	int stride = 32;
+	int nVecs = 9 ; // has to be odd
+	GroundVectors ground_vecs(nVecs, stride, AGENT_WIDTH);
+	ground_vecs.cloud = plane_detection.cloud;
 
-				// this is the correct way of checking for membership of the vertex/pixel in the groundVertices,
-				// but we are using the mask instead because its easier to clean up the mask than it is the cloud 
-				//std::vector<int> groundVertices = plane_detection.plane_vertices_[surfaces.groundIdx];
-				//int pixelIdx = i * plane_detection.cloud.width() + col;
-				//auto inclusion = std::find(groundVertices.begin(), groundVertices.end(), pixelIdx);
-				//if (inclusion != groundVertices.end()) {
-
-				// Check if the pixel value is approximately green
-				if (std::abs(pixelValue[1] - 255) < 20 && pixelValue[0] < 20 && pixelValue[2] < 20) {
-					if (!onSurface){
-						onSurface=true;
-					}
-					double x, y, z;
-					plane_detection.cloud.get(i, maxJ, x, y, z);
-
-					if (z > maxZ){
-						maxZ = z;
-						maxI = i;
-					}
-
-				// if we are on surface and have reached a pixel of another class, break early
-				}else if (onSurface){
-					break;
-				}
-			}
-
-			// Ensure maxI was updated
-			if (maxI != -1) {
-				// Define start and end points for the arrow
-				cv::Point start(maxJ,  plane_detection.cloud.height()-1);
-				cv::Point end(maxJ, maxI);
-
-				// Draw the arrowed line
-				cv::arrowedLine(drawnImage, start, end, color, thickness, lineType, shift, tipLength);
-			}
-
-			}
-
+	// Create a vector to store maxJ values
+	int col = plane_detection.cloud.width() / 2 - 1;
+    std::vector<int> maxJValues;
+    for (int j = col-(nVecs/2)*stride; j <=col+(nVecs/2)*stride; j += stride) {
+        maxJValues.push_back(j);
     }
+
+	for (int k; k < maxJValues.size(); k++){
+		int maxJ = maxJValues[k];
+		bool onSurface = false;
+		double maxZ = 0;
+		int maxI = -1;
+		for (int i = plane_detection.cloud.height()-1; i >= 0; i--){
+			// this is the correct way of checking for membership of the vertex/pixel in the groundVertices,
+			// but we are using the mask instead because its easier to clean up the mask than it is the cloud 
+			//std::vector<int> groundVertices = plane_detection.plane_vertices_[surfaces.groundIdx];
+			//int pixelIdx = i * plane_detection.cloud.width() + col;
+			//auto inclusion = std::find(groundVertices.begin(), groundVertices.end(), pixelIdx);
+			//if (inclusion != groundVertices.end()) {
+
+			// Check if the pixel value is approximately green
+			cv::Vec3b pixelValue = image.at<cv::Vec3b>(i, maxJ);
+			if (std::abs(pixelValue[1] - 255) < 20 && pixelValue[0] < 20 && pixelValue[2] < 20) {
+				if (!onSurface){
+					onSurface=true;
+				}
+				double x, y, z;
+				plane_detection.cloud.get(i, maxJ, x, y, z);
+
+				if (z > maxZ){
+					maxZ = z;
+					maxI = i;
+					ground_vecs.vecMaxI[k] = i; 
+					ground_vecs.vecMaxJ[k] = maxJ; 
+				}
+
+			// if we are on surface and have reached a pixel of another class, break early
+			}else if (onSurface){
+				break;
+			}
+		}
+
+		// Ensure maxI was updated
+		if (maxI != -1) {
+			// Define start and end points for the arrow
+			cv::Point start(maxJ,  plane_detection.cloud.height()-1);
+			cv::Point end(maxJ, maxI);
+
+			// Draw the arrowed line
+			cv::arrowedLine(drawnImage, start, end, color, thickness, lineType, shift, tipLength);
+		}
+
+	}
+	ground_vecs.finalize(image);
+
+	for (int k; k < maxJValues.size(); k++){
+		// Ensure maxI was updated
+		// Define start and end points for the arrow
+		cv::Point start(ground_vecs.vecMaxJ[k],  plane_detection.cloud.height()-1);
+		cv::Point end(ground_vecs.vecMaxJ[k], ground_vecs._vecMaxI[k]);
+
+		// Draw the arrowed line
+		cv::Scalar violet(255, 0, 255);
+		cv::arrowedLine(drawnImage, start, end, violet, thickness, lineType, shift, tipLength);
+
+	}
+
 	return drawnImage;
 }
 
-int main() {
-
-	PlaneDetection plane_detection;
+int realSenseAttached(){
 
     try {
         RealSense realsense;
         realsense.startPipeline();
 
-        realsense.captureFrames([&plane_detection](const rs2::frameset& frames) {
+        realsense.captureFrames([](const rs2::frameset& frames) {
             rs2::depth_frame depth = frames.get_depth_frame();
             rs2::video_frame rgb_frame = frames.get_color_frame();
 
@@ -260,15 +427,6 @@ int main() {
 
 			cv::Mat drawnImage = drawDistanceVectors(plane_detection.seg_img_, plane_detection, surfaces);
 
-			//std::cout << plane_detection.cloud.vertices.size() << std::endl;
-			//std::cout << total << std::endl;
-
-            //plane_detection.prepareForMRF();
-            //runMRFOptimization();
-
-            // Display the frame
-            //cv::imshow("Processed Frame", color_mat);
-
 			cv::imshow("Processed Frame", drawnImage);
             if (cv::waitKey(1) == 27) { // Exit on ESC key
 				cv::imwrite("sample_segmentation.png", plane_detection.seg_img_);
@@ -286,5 +444,61 @@ int main() {
         return EXIT_FAILURE;
     }
 
+	return EXIT_SUCCESS;
+}
+
+int loadProcessDefault(){
+	std::string colorImagePath = "data/raw_image.png";
+    std::string depthImagePath = "data/depth_image.png";
+
+	cv::Mat colorImage = cv::imread(colorImagePath, cv::IMREAD_COLOR);
+	cv::Mat depthImage = cv::imread(depthImagePath, cv::IMREAD_UNCHANGED);
+
+	plane_detection.readDepthImage(depthImage);
+	plane_detection.readColorImage(colorImage);
+
+	// removed the plotting and moved to public member so we can change
+	// the colors and plot according to the logic out here
+	plane_detection.runPlaneDetection();
+
+	Surfaces surfaces(plane_detection);
+
+	plane_detection.plane_filter.colors = surfaces.colors;
+	plane_detection.plane_filter.publicRefineDetails(&plane_detection.plane_vertices_, nullptr, &plane_detection.seg_img_);
+	plane_detection.plane_filter.colors = {};
+
+	cv::Mat drawnImage = drawDistanceVectors(plane_detection.seg_img_, plane_detection, surfaces);
+
+	// Display the image in a loop
+    while (true) {
+        cv::imshow("Processed Frame", drawnImage);
+        
+        // Wait for 1 ms and check if the ESC key is pressed
+        int key = cv::waitKey(1);
+        if (key == 27) { // ESC key has ASCII code 27
+            break;
+        }
+    }
+
+    // Clean up and close the window
+    cv::destroyAllWindows();
+	return EXIT_SUCCESS;
+}
+
+int main(int argc, char* argv[]) {
+ 	if (argc < 2) {
+        int response = realSenseAttached();
+        return response;
+    } else {
+        std::string arg2 = argv[1]; // Convert the C-style string to a std::string
+
+        if (arg2 == "-nocam") {
+            int response = loadProcessDefault();
+            return response;
+        } else {
+            std::cerr << "Usage: " << argv[0] << " <-nocam>, or execute without arguments" << std::endl;
+			return EXIT_FAILURE;
+        }
+    }
     return EXIT_SUCCESS;
 }
