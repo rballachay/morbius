c956d9cb0ad97a4495d5d2a3d86e1b6858745f02
rballachay <riley.ballachay@gmail.com>
2024-07-30 15:29:14 -0400

    update vision build

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # morbius
 Code for Robot Vision + Language Project w Dr. Joseph Vybihal
 
-NOTE! REMEMBER that you need to re-name the submodule StyleTTS2/utils.py to StyleTTS2/munch_utils.py to avoid a name conflict with utils elsewhere.
+
--- vision/build.sh
+++ vision/build.sh
@@ -1,7 +1,57 @@
 #!/bin/bash
+
+# INTO SUBMODULE
+cd submodules/ORB_SLAM3
+
+echo "Configuring and building Thirdparty/DBoW2 ..."
+
+cd Thirdparty/DBoW2
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j
+
+cd ../../g2o
+
+echo "Configuring and building Thirdparty/g2o ..."
+
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j
+
+cd ../../Sophus
+
+echo "Configuring and building Thirdparty/Sophus ..."
+
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j
+
+cd ../../../
+
+echo "Uncompress vocabulary ..."
+
+cd Vocabulary
+tar -xf ORBvoc.txt.tar.gz
+cd ..
+
+echo "Configuring and building ORB_SLAM3 ..."
+
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j4
+
+
+# OUT OF SUBMODULE
+cd ../../
+
+echo "Configuring and building vision ..."
+
 mkdir -p build
 cd build
 cmake .. 
-cd build 
 make
 cd ../
